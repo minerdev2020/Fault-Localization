d0bb0159bd7ce510ab795eeb22efe81fb225246f
minerdev <minerdev2020@gmail.com>
2021-04-22 14:17:30 +0900

    style: 코드 정리

--- app/src/main/java/com/minerdev/faultlocalization/view/fragment/EquipmentInfoFragment.kt
+++ app/src/main/java/com/minerdev/faultlocalization/view/fragment/EquipmentInfoFragment.kt
@@ -1,6 +1,5 @@
 package com.minerdev.faultlocalization.view.fragment
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.minerdev.faultlocalization.databinding.FragmentEquipmentInfoDataBinding
 import com.minerdev.faultlocalization.model.Equipment
-import com.minerdev.faultlocalization.utils.Constants.BASE_URL
 
 class EquipmentInfoFragment(private val equipment: Equipment) : Fragment() {
     private val binding by lazy { FragmentEquipmentInfoDataBinding.inflate(layoutInflater) }
@@ -20,14 +18,10 @@ class EquipmentInfoFragment(private val equipment: Equipment) : Fragment() {
         return binding.root
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.tvName.text = equipment.name
         binding.tvState.text = equipment.state.name
-
-        binding.webView.settings.javaScriptEnabled = true
-        binding.webView.loadUrl(BASE_URL)
     }
 }
