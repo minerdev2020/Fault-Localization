a1984a183804566382f381cd9b015b0b535e0d07
minerdev <minerdev2020@gmail.com>
2021-05-06 21:24:45 +0900

    feat: 센서 그래프에 과거 기록 열람 기능 대부분 완성
    - 표시 단위 설정 기능 추가, 1초, 1분, 1시간, 1일, 1달 중에서 고를수있다
    - 기간 선택 기능 추가
    - 필요한 네트워크 등 관련 코드 추가
    - 필요 없는 기능 비활성화

--- app/src/main/java/com/minerdev/faultlocalization/view/fragment/SensorDataPageFragment.kt
+++ app/src/main/java/com/minerdev/faultlocalization/view/fragment/SensorDataPageFragment.kt
@@ -15,13 +15,13 @@ import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.MarkerView
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.utils.MPPointF
+import com.minerdev.faultlocalization.R
 import com.minerdev.faultlocalization.databinding.FragmentSensorDataBinding
 import com.minerdev.faultlocalization.model.Sensor
 import com.minerdev.faultlocalization.model.SensorData
@@ -37,12 +37,24 @@ import java.net.URISyntaxException
 import java.util.*
 import kotlin.collections.ArrayList
 
-
 class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
     private val binding by lazy { FragmentSensorDataBinding.inflate(layoutInflater) }
     private var socket: Socket? = null
+    private var timeOffset = 0L
+    private var samplingTime = 1
     private var fromDate = ""
     private var toDate = ""
+
+    class MyMarkerView(context: Context, layoutResource: Int) :
+        MarkerView(context, layoutResource) {
+        override fun refreshContent(e: Entry?, highlight: Highlight?) {
+            super.refreshContent(e, highlight)
+        }
+
+        override fun getOffset(): MPPointF {
+            return MPPointF((-(width / 2)).toFloat(), (-height).toFloat())
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -59,58 +71,8 @@ class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
         binding.tvFrom.text = "全部"
         binding.tvTo.text = "全部"
 
-        binding.tvFrom.setOnClickListener {
-            val dialog = DatePickerDialogFragment()
-            dialog.listener = View.OnClickListener {
-                binding.tvFrom.text = dialog.date
-                if (dialog.date == "全部") {
-                    fromDate = ""
-                    socket?.emit("onDateChanged", buildJsonObject {
-                        put("from", fromDate)
-                        put("to", toDate)
-                    })
-                } else {
-                    fromDate = getUnixTime(dialog.date).toString()
-                    socket?.emit("onDateChanged", buildJsonObject {
-                        put("from", fromDate)
-                        put("to", toDate)
-                    })
-                }
-            }
-            dialog.show(requireActivity().supportFragmentManager, "DatePickerDialogFragment")
-        }
-        binding.tvTo.setOnClickListener {
-            val dialog = DatePickerDialogFragment()
-            dialog.listener = View.OnClickListener {
-                binding.tvTo.text = dialog.date
-                if (dialog.date == "全部") {
-                    toDate = ""
-                    socket?.emit("onDateChanged", buildJsonObject {
-                        put("from", fromDate)
-                        put("to", toDate)
-                    })
-                } else {
-                    toDate = getUnixTime(dialog.date).toString()
-                    socket?.emit("onDateChanged", buildJsonObject {
-                        put("from", fromDate)
-                        put("to", toDate)
-                    })
-                }
-            }
-            dialog.show(requireActivity().supportFragmentManager, "DatePickerDialogFragment")
-        }
-
-        binding.btnReset.setOnClickListener {
-            binding.tvFrom.text = "全部"
-            binding.tvTo.text = "全部"
-            fromDate = ""
-            toDate = ""
-            socket?.emit("onDateChanged", buildJsonObject {
-                put("from", fromDate)
-                put("to", toDate)
-            })
-        }
-
+        setSamplingTimeSelector()
+        setDateRangeSelector()
         setChart(binding.chart)
 
         try {
@@ -124,24 +86,31 @@ class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
             s.connect()
             s.on(Socket.EVENT_CONNECT) {
                 Log.d(Constants.TAG, "Connected!")
-                s.emit("start", sensor.id)
+                s.emit("start", buildJsonObject {
+                    put("sensor_id", sensor.id)
+                    put("sampling_time", samplingTime)
+                })
             }
         }
 
         socket?.on("onInit") { response ->
-            val jsonResponse = JSONObject(response[0].toString())
-            initData(
-                binding.chart,
-                Json.decodeFromString(jsonResponse.getString("initData"))
-            )
+            if (!response.isNullOrEmpty()) {
+                val jsonResponse = JSONObject(response[0].toString())
+                initData(
+                    binding.chart,
+                    Json.decodeFromString(jsonResponse.getString("initial_data"))
+                )
+            }
         }
 
         socket?.on("onNewData") { response ->
-            Log.d(Constants.TAG, response[0].toString())
-            addData(
-                binding.chart,
-                Json.decodeFromString(response[0].toString())
-            )
+            if (!response.isNullOrEmpty()) {
+                Log.d(Constants.TAG, response[0].toString())
+                addData(
+                    binding.chart,
+                    Json.decodeFromString(response[0].toString())
+                )
+            }
         }
     }
 
@@ -150,27 +119,35 @@ class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
         socket?.disconnect()
     }
 
-    private fun getUnixTime(time: String): Int {
+    private fun getUnixTime(time: String): Long {
         val format = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.getDefault())
         val date = format.parse(time)
-        return (date.time / 1000).toInt()
+        return date.time
     }
 
     private fun getHMS(time: Long): String {
-        val date = Date(time * 1000)
-        Log.d(Constants.TAG, "$time $date")
+        val date = Date(time)
         val format = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(date)
         return format.toString()
     }
 
-    private fun addData(chart: LineChart, sensorData: SensorData) {
-        val data = chart.data
-        data.addEntry(Entry(sensorData.time.toFloat(), sensorData.value), 0)
-        data.notifyDataChanged()
+    private fun getYMD(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date)
+        return format.toString()
+    }
 
-        chart.notifyDataSetChanged()
-        chart.setVisibleXRangeMaximum(100F)
-        chart.moveViewTo(sensorData.time.toFloat(), 50F, YAxis.AxisDependency.LEFT)
+    private fun addData(chart: LineChart, sensorData: SensorData) {
+        val set = chart.data.getDataSetByIndex(0)
+        set.removeEntry(0)
+        set.addEntry(
+            Entry(
+                ((sensorData.time - timeOffset) / 1000 / samplingTime).toFloat(),
+                sensorData.value
+            )
+        )
+        chart.setVisibleXRangeMaximum(60F)
+        chart.moveViewToX(((sensorData.time - timeOffset) / 1000 / samplingTime).toFloat())
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -180,18 +157,14 @@ class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
             setDrawGridBackground(true)
             setBackgroundColor(Color.BLACK)
             setGridBackgroundColor(Color.BLACK)
-
-            setTouchEnabled(true)
-
-            isDragEnabled = true
-            isDragDecelerationEnabled = true
-            setScaleEnabled(true)
-
             isAutoScaleMinMaxEnabled = true
 
-            setPinchZoom(true)
-
-            description.isEnabled = true
+            setTouchEnabled(true)
+            setScaleEnabled(false)
+            setPinchZoom(false)
+            isDragEnabled = false
+            isDragDecelerationEnabled = false
+            isDoubleTapToZoomEnabled = false
 
             setOnTouchListener { _, event ->
                 when (event.action) {
@@ -210,16 +183,16 @@ class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
         }
 
         with(chart.description) {
-            isEnabled = true
+            isEnabled = false
             text = "sensor data"
-            textSize = 15f
+            textSize = 15F
             textColor = Color.WHITE
         }
 
         with(chart.legend) {
-            isEnabled = true
-            formSize = 10f
-            textSize = 12f
+            isEnabled = false
+            formSize = 10F
+            textSize = 12F
             textColor = Color.WHITE
         }
 
@@ -231,10 +204,15 @@ class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
             position = XAxis.XAxisPosition.BOTTOM
             setDrawGridLines(true)
             setDrawAxisLine(true)
+            setLabelCount(5, false)
 
             valueFormatter = object : ValueFormatter() {
-                override fun getFormattedValue(value: Float, axis: AxisBase?): String {
-                    return getHMS(value.toLong())
+                override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+                    val time = value.toLong() * 1000 * samplingTime + timeOffset
+                    return if (samplingTime <= 60)
+                        getHMS(time)
+                    else
+                        getYMD(time)
                 }
             }
         }
@@ -254,19 +232,24 @@ class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
     }
 
     private fun initData(chart: LineChart, sensorDataList: List<SensorData>) {
-        with(chart) {
-            clear()
-            data = LineData(createSet(sensorDataList))
-            data.notifyDataChanged()
-            notifyDataSetChanged()
-            invalidate()
+        if (sensorDataList.isNotEmpty()) {
+            timeOffset = sensorDataList[0].time
+            with(chart) {
+                clear()
+                data = LineData(createSet(sensorDataList))
+            }
         }
     }
 
     private fun createSet(sensorDataList: List<SensorData>): LineDataSet {
         val entryList = ArrayList<Entry>()
-        for ((i, sensorData) in sensorDataList.withIndex()) {
-            entryList.add(Entry(sensorData.time.toFloat(), sensorData.value))
+        for (sensorData in sensorDataList) {
+            entryList.add(
+                Entry(
+                    ((sensorData.time - timeOffset) / 1000 / samplingTime).toFloat(),
+                    sensorData.value
+                )
+            )
         }
 
         return LineDataSet(entryList, "value").apply {
@@ -280,14 +263,160 @@ class SensorDataPageFragment(private val sensor: Sensor) : Fragment() {
         }
     }
 
-    class MyMarkerView(context: Context, layoutResource: Int) :
-        MarkerView(context, layoutResource) {
-        override fun refreshContent(e: Entry?, highlight: Highlight?) {
-            super.refreshContent(e, highlight)
+    private fun setSamplingTimeSelector() {
+        binding.btnOneSec.setOnClickListener {
+            samplingTime = 1
+            socket?.emit("onSamplingTimeChanged", buildJsonObject {
+                put("sensor_id", sensor.id)
+                put("sampling_time", samplingTime)
+            })
         }
 
-        override fun getOffset(): MPPointF {
-            return MPPointF((-(width / 2)).toFloat(), (-height).toFloat())
+        binding.btnOneMin.setOnClickListener {
+            samplingTime = 60
+            socket?.emit("onSamplingTimeChanged", buildJsonObject {
+                put("sensor_id", sensor.id)
+                put("sampling_time", samplingTime)
+            })
+        }
+
+        binding.btnOneHour.setOnClickListener {
+            samplingTime = 3600
+            socket?.emit("onSamplingTimeChanged", buildJsonObject {
+                put("sensor_id", sensor.id)
+                put("sampling_time", samplingTime)
+            })
+        }
+
+        binding.btnOneDay.setOnClickListener {
+            samplingTime = 3600 * 24
+            socket?.emit("onSamplingTimeChanged", buildJsonObject {
+                put("sensor_id", sensor.id)
+                put("sampling_time", samplingTime)
+            })
+        }
+
+        binding.btnOneMonth.setOnClickListener {
+            samplingTime = 3600 * 24 * 30
+            socket?.emit("onSamplingTimeChanged", buildJsonObject {
+                put("sensor_id", sensor.id)
+                put("sampling_time", samplingTime)
+            })
+        }
+    }
+
+    private fun getCurrentSamplingTime(): Int {
+        return when (binding.toggleButtonGroup.checkedButtonId) {
+            R.id.btn_one_sec -> 1
+            R.id.btn_one_min -> 60
+            R.id.btn_one_hour -> 3600
+            R.id.btn_one_day -> 3600 * 24
+            R.id.btn_one_month -> 3600 * 24 * 30
+            else -> 3600 * 24 * 30
+        }
+    }
+
+    private fun calculateSamplingTime(from: Long, to: Long): Int {
+        var diffTime = (to - from) / 1000
+        Log.d(Constants.TAG, "$from $to $diffTime")
+        return when {
+            diffTime < 60 -> 1
+            60.let { diffTime /= it; diffTime } < 60 -> 60
+            60.let { diffTime /= it; diffTime } < 24 -> 3600
+            24.let { diffTime /= it; diffTime } < 30 -> 3600 * 24
+            30.let { diffTime /= it; diffTime } < 12 -> 3600 * 24 * 30
+            else -> 3600 * 24 * 30
+        }
+    }
+
+    private fun setDateRangeSelector() {
+        binding.tvFrom.setOnClickListener {
+            val dialog = DatePickerDialogFragment()
+            dialog.listener = View.OnClickListener {
+                binding.tvFrom.text = dialog.date
+                if (dialog.date == "全部") {
+                    fromDate = ""
+                    samplingTime = if (toDate == "") {
+                        getCurrentSamplingTime()
+                    } else {
+                        calculateSamplingTime(System.currentTimeMillis(), toDate.toLong())
+                    }
+
+                    socket?.emit("onDateChanged", buildJsonObject {
+                        put("sensor_id", sensor.id)
+                        put("sampling_time", samplingTime)
+                        put("from", fromDate)
+                        put("to", toDate)
+                    })
+
+                } else {
+                    fromDate = getUnixTime(dialog.date).toString()
+                    samplingTime = if (toDate == "") {
+                        calculateSamplingTime(fromDate.toLong(), System.currentTimeMillis())
+                    } else {
+                        calculateSamplingTime(fromDate.toLong(), toDate.toLong())
+                    }
+
+                    socket?.emit("onDateChanged", buildJsonObject {
+                        put("sensor_id", sensor.id)
+                        put("sampling_time", samplingTime)
+                        put("from", fromDate)
+                        put("to", toDate)
+                    })
+                }
+            }
+            dialog.show(requireActivity().supportFragmentManager, "DatePickerDialogFragment")
+        }
+        binding.tvTo.setOnClickListener {
+            val dialog = DatePickerDialogFragment()
+            dialog.listener = View.OnClickListener {
+                binding.tvTo.text = dialog.date
+                if (dialog.date == "全部") {
+                    toDate = ""
+                    samplingTime = if (fromDate == "") {
+                        getCurrentSamplingTime()
+                    } else {
+                        calculateSamplingTime(fromDate.toLong(), System.currentTimeMillis())
+                    }
+
+                    socket?.emit("onDateChanged", buildJsonObject {
+                        put("sensor_id", sensor.id)
+                        put("sampling_time", samplingTime)
+                        put("from", fromDate)
+                        put("to", toDate)
+                    })
+
+                } else {
+                    toDate = getUnixTime(dialog.date).toString()
+                    samplingTime = if (toDate == "") {
+                        calculateSamplingTime(System.currentTimeMillis(), toDate.toLong())
+                    } else {
+                        calculateSamplingTime(fromDate.toLong(), toDate.toLong())
+                    }
+
+                    socket?.emit("onDateChanged", buildJsonObject {
+                        put("sensor_id", sensor.id)
+                        put("sampling_time", samplingTime)
+                        put("from", fromDate)
+                        put("to", toDate)
+                    })
+                }
+            }
+            dialog.show(requireActivity().supportFragmentManager, "DatePickerDialogFragment")
+        }
+
+        binding.btnReset.setOnClickListener {
+            binding.tvFrom.text = "全部"
+            binding.tvTo.text = "全部"
+            fromDate = ""
+            toDate = ""
+            samplingTime = getCurrentSamplingTime()
+            socket?.emit("onDateChanged", buildJsonObject {
+                put("sensor_id", sensor.id)
+                put("sampling_time", samplingTime)
+                put("from", fromDate)
+                put("to", toDate)
+            })
         }
     }
 }
